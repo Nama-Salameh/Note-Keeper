331bd3cc54ccdeda921c5ab9944fec9d0198aa0a
Nama' Salameh <nama.salameh@gmail.com>
2023-09-08 11:12:32 +0300

    Refactor getNote

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Note = require("../models/note");
 
-
 router.get("/", async (req, res) => {
   try {
     const notes = await Note.find();
@@ -12,27 +11,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-
-router.get("/:id", async (req, res) => {
-    try {
-      const note = await Note.findOne();
-      res.json(note);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+router.get("/:id", getNote, (req, res) => {
+    res.json(res.note);
+});
 
 router.post("/", async (req, res) => {
-    const note = new Note({
-        title: req.body.title,
-        content: req.body.content,
-    })
-try{
+  const note = new Note({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
     const newNote = await note.save();
     res.status(201).json(newNote);
-}catch(error){
+  } catch (error) {
     res.status(400).json({ message: error.message });
-}
+  }
 });
+
+async function getNote(req, res, next) {
+  let note;
+  try {
+    note = await Note.findById(req.params.id);
+    if (note == null)
+      return res.status(404).json({ message: "Connot find note" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+  res.note = note;
+  next();
+}
 
 module.exports = router;
